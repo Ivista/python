05342b334eac67a5b27a003215455fab9d0f0d96
Ivista <bevanc6@gmail.com>
2020-05-02 06:27:39 +0100

    Pyinputplus min max greaterthan lessthan  and Blank input

--- inputValidation.py
+++ inputValidation.py
@@ -35,3 +35,34 @@ responce = pyip.inputInt(prompt='Enter a Number!')
 print(responce)
 
 help(pyip.inputChoice)
+
+#-----------------------------------------------------------------------------------------------
+
+# The inputNum(), inputInt(), and inputFloat() functions, which accept int and float numbers, also have min, max, greaterThan, and lessThan keyword arguments for specifying a range of valid values. For example, enter the following into the interactive shell:
+
+
+responce = pyip.inputNum('Enter num: ', min=4)
+
+print(responce)
+
+responce = pyip.inputNum('Enter num: ', greaterThan=4)
+
+print(responce)
+
+responce = pyip.inputNum('Enter num: ', min=4, lessThan=6)
+
+print(responce)
+
+
+
+#----------------------------------------------------------------
+
+# The inputNum(), inputInt(), and inputFloat() functions, which accept int and float numbers, also have min, max, greaterThan, and lessThan keyword arguments for specifying a range of valid values. For example, enter the following into the interactive shell:
+
+responce = pyip.inputNum('Enter Num: ')
+print(responce)
+
+responce = pyip.inputNum('Enter Num: ', blank=True)
+print(responce)
+
+# Use blank = True if you’d like to make input optional so that the user doesn’t need to enter anything.
