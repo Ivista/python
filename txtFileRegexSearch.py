327652416a769b92803155d68706e0f6731691f6
Ivista <bevanc6@gmail.com>
2020-07-19 14:38:53 +0100

    Added progress bar for xlsx and tested with .txt and .xlsx files

--- txtFileRegexSearch.py
+++ txtFileRegexSearch.py
@@ -1,8 +1,8 @@
 # TODO Walk directory path added need to test, update all print to console and check logfile entries, testwith strongpassword Regex. 
 
 
-'''Added timestamp to log file also added start and end tags to log.  Need to test further with some more RegEx patterns and then look to 
-test in production. '''
+'''Added memory tuneing to speed up time to completion of script(cut sript time to one quarter).  Added colored 'FOUND' flags to terminal output. Tested with two large spreadsheets.  Need to 
+to test with a few .txt files.  Look at progress bars!(Now added need to look at some description text) '''
 
 from datetime import datetime
 from pathlib import Path
@@ -11,6 +11,9 @@ import re
 import chardet
 import openpyxl
 from openpyxl.utils import get_column_letter
+from termcolor import colored
+from tqdm import tqdm
+import colorama
 
 
 def timestamp():
@@ -26,9 +29,10 @@ results_log.write('####Start txtFileRegexSearch Script####   \n')
 results_log.close()
 
 # TODO: open all *.txt files in a directory 
-userRegex = re.compile(r'Get\w{5}')
+userRegex = re.compile(r'IB\w{10}')
 
-for folderName, subfolders, filenames in os.walk('C:\\test\\'): # Iterate through folder structure under this path. 
+# Iterate through folder structure under this path.
+for folderName, subfolders, filenames in os.walk('C:\\test'):
     print(f'Searching Folder: {folderName} for .txt and .xlsx files')
     timestamp()
     results_log = open('resultsLog.txt', 'a')
@@ -81,6 +85,7 @@ for folderName, subfolders, filenames in os.walk('C:\\test\\'): # Iterate throug
                 reg_Match = userRegex.findall(i)
                 
                 for regex_lineCount, txtLine_regMatch in enumerate(reg_Match, start=1):
+                    print(colored('FOUND REGEX PATTERN ', 'green'), end='')
                     print(f'File {testFileObject}, Line: {c+1}, contains string: {regex_lineCount}--{txtLine_regMatch} ')
                     timestamp()
                     results_log = open('resultsLog.txt', 'a')
@@ -104,7 +109,7 @@ for folderName, subfolders, filenames in os.walk('C:\\test\\'): # Iterate throug
         results_log = open('resultsLog.txt', 'a')
         results_log.write(f'xlsx file found: {str(testExcelObject)}  \n')
         results_log.close()
-        wb = openpyxl.load_workbook(testExcelObject)
+        wb = openpyxl.load_workbook(filename= testExcelObject, read_only=True)
         sheet_Names = wb.sheetnames
 
         for unique_Sheet in sheet_Names:
@@ -115,25 +120,50 @@ for folderName, subfolders, filenames in os.walk('C:\\test\\'): # Iterate throug
             results_log.close()
             sheet_Search = wb[unique_Sheet]
                 
+            # Pre memory tuneing, new block below this one. 
+            # for uni_row in range(1, sheet_Search.max_row+1):
+            #     #myList.append([])  # Load new nested list index is referenced below.
+            #     for cellVal2 in range(1, sheet_Search.max_column+1):
+            #         print(uni_row)#(sheet_Search.cell(row=uni_row, column=cellVal2).value)
+            #         search_Pattern = sheet_Search.cell(row=uni_row, column=cellVal2).value
+            #         if search_Pattern:
 
-            for uni_row in range(1, sheet_Search.max_row+1):
-                #myList.append([])  # Load new nested list index is referenced below.
-                for cellVal2 in range(1, sheet_Search.max_column+1):
-                    #print(sheet_Search.cell(row=uni_row, column=cellVal2).value)
-                    search_Pattern = sheet_Search.cell(row=uni_row, column=cellVal2).value
+            #             if userRegex.findall(search_Pattern):
+            #                 reg_Match = userRegex.findall(search_Pattern)
+            #                 column_letter = get_column_letter(cellVal2)
+            #                 file_hasReg = True
+
+            #                 for match_inList in reg_Match:
+            #                     timestamp()
+            #                     print(f'File {testExcelObject}, Sheet:{unique_Sheet}, row: {uni_row}, column: {column_letter} contains string: {match_inList}')
+            #                     results_log = open('resultsLog.txt', 'a')
+            #                     results_log.write(f'File {testExcelObject}, Sheet:{unique_Sheet}, row: {uni_row}, column: {column_letter} contains string: {match_inList} \n')
+            #                     results_log.close()
+            howmany_rows = wb[unique_Sheet].max_row
+            for uni_row in tqdm(wb[unique_Sheet].rows, total=howmany_rows):
+                for cellVal2 in uni_row:
+                    search_Pattern = cellVal2.value
+                    # if search_Pattern != None:
+                    #     print(search_Pattern)
+                    #     print(cellVal2.coordinate)
                     if search_Pattern:
 
                         if userRegex.findall(search_Pattern):
                             reg_Match = userRegex.findall(search_Pattern)
-                            column_letter = get_column_letter(cellVal2)
+                            column_letter = cellVal2.coordinate
                             file_hasReg = True
 
                             for match_inList in reg_Match:
                                 timestamp()
-                                print(f'File {testExcelObject}, Sheet:{unique_Sheet}, row: {uni_row}, column: {column_letter} contains string: {match_inList}')
+                                print(colored('\nFOUND REGEX PATTERN ', 'green'), end='')
+                                print(f'File {testExcelObject}, Sheet:{unique_Sheet}, Cell: {column_letter} contains string: {match_inList}')
                                 results_log = open('resultsLog.txt', 'a')
-                                results_log.write(f'File {testExcelObject}, Sheet:{unique_Sheet}, row: {uni_row}, column: {column_letter} contains string: {match_inList} \n')
+                                results_log.write(f'File {testExcelObject}, Sheet:{unique_Sheet}, Cell: {column_letter} contains string: {match_inList} \n')
                                 results_log.close()
+
+
+
+
         if file_hasReg == False:
             print(f'No regex found in: {testExcelObject}')
             timestamp()
