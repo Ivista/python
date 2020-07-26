82cdcafa2db7dc1e4a37726a0d6567c6487155e0
Ivista <bevanc6@gmail.com>
2020-07-26 18:16:50 +0100

    upload of backupToZip.py

---
+++ backupToZip.py
@@ -0,0 +1,54 @@
+#! python3
+# backupToZip.py - Copies an entire folder and its contents into
+# a ZIP file whose filename increments.
+
+import zipfile
+import os
+
+
+def backupToZip(folder):
+    # Back up the entire contest of 'folder' into a ZIP file.
+
+    folder = os.path.abspath(folder)
+
+    # Change directory of files to backup into!
+
+    os.chdir('C:\\Backups')
+
+    #figure out teh filename this code should use based on what files already exist.
+
+    number = 1
+
+    while True:
+        zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
+        if not os.path.exists(zipFilename):
+            break
+
+        number = number + 1
+
+    # Create the ZIP file
+
+    print(f'Creating {zipFilename}....')
+    backupZip = zipfile.ZipFile(zipFilename, 'w')
+
+    # TODO: Walk the enire folder tree and compres the files in each folder.
+
+    for foldername, subfolders, filenames in os.walk(folder):
+        print(f'Adding files in {foldername}...')
+        #Add the current folder to the ZIP file
+        backupZip.write(foldername)
+
+        # Add all the files in this folder to the ZIP file.
+
+        for filename in filenames:
+            newBase = os.path.basename(folder) + '_'
+            if filename.startswith(newBase) and filename.endswith('.zip'):
+                continue  # don't back up the backup ZIP files.
+            backupZip.write(os.path.join(foldername, filename))
+
+    backupZip.close()
+
+    print('Done.')
+
+
+backupToZip('C:\\delicious')
